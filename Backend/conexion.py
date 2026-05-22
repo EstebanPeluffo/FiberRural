20523e895aab8fbdb48b8a6569986b5dbf017304
Esteban Peluffo <estebanjavierpeluffo@gmail.com>
2026-05-22 14:38:37 -0500

    Conexion.py

--- Backend/conexion.py
+++ Backend/conexion.py
@@ -2,10 +2,11 @@ import pymysql
 
 def get_db_connection():
     connection = pymysql.connect(
-        host="localhost",
-        user="root",
-        password="root",
-        database="fiberRural",
+        host="sql10.freesqldatabase.com",
+        user="sql10827401",
+        password="6FmDYnAlHQ",
+        database="sql10827401",
+        port=3306,
         cursorclass=pymysql.cursors.DictCursor
     )
     return connection
