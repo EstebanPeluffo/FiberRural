8705bfe61f26781183c9bb2685474394cd0b8954
Esteban Peluffo <estebanjavierpeluffo@gmail.com>
2026-05-22 20:41:28 -0500

    detallen en el login

--- Frontend/lib/screens/login.dart
+++ Frontend/lib/screens/login.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'dart:async';
+//import 'dart:async';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'MenuPrincipal.dart';
 import 'RegistroUsuario.dart';
