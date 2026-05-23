33f7369cc3ce9ae43d32494df2c2c3585c347b6e
Esteban Peluffo <estebanjavierpeluffo@gmail.com>
2026-05-23 00:39:38 -0500

    corrigiendo errores de IP

--- Frontend/lib/screens/recuperarPassword.dart
+++ Frontend/lib/screens/recuperarPassword.dart
@@ -109,7 +109,9 @@ class _PantallaOlvidePasswordState extends State<PantallaOlvidePassword> {
     setState(() => _cargando = true);
 
     try {
-      final url = Uri.parse("http://192.168.100.31:8000/cambiar-password");
+      final url = Uri.parse(
+        "https://fiberrural-api.onrender.com/cambiar-password",
+      );
       final response = await http.post(
         url,
         headers: {"Content-Type": "application/json"},
